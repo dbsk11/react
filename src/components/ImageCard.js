c5681ac1851cd92fbb7b9e4cd6386693e1284ced
Jessica Kim <jessikim807@gmail.com>
2020-11-12 08:32:33 -0500

    created components

---
+++ src/components/ImageCard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const ImageCard = (props) => {
+
+
+  
+  return(
+    <div>
+      <h1>ImageCard</h1>
+    </div>
+  );
+};
+
+export default ImageCard;
---
+++ src/components/MainContainer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ImageCard from './ImageCard';
+
+const MainContainer = (props) => {
+  
+
+
+  return (
+    <div>
+
+    </div>
+  );
+};
+
+export default MainContainer;
